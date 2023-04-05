f8c638eea4b9e8d7a96efb07baa682f12a82f68c
teamidol <laurencemy@gmail.com>
2023-04-04 19:40:58 -0700

    had gituation, just added back clear.js to my files

--- clear.js
+++ clear.js
@@ -1,16 +1,18 @@
 'use strict';
-//******** (this came from lab 12 -- laurence) */
-require('dotenv').config();
+//*********** (this came from lab 12 -- laurence)
+
+require ('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_URL);
 
 const Game = require('./models/game');
 
-async function clear() {
+async function clear () {
   try {
     await Game.deleteMany({});
-    console.log('books cleared from DB'); // <<< not sure if this is needed
+    console.log('games cleared from DB'); // <<<< not sure if this is needed
+
   } catch (err) {
     console.error(err);
   } finally {
