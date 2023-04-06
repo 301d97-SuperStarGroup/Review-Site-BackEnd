03eaed86af1e3349f3fccc55ced37c13b3e36e20
teamidol <laurencemy@gmail.com>
2023-04-06 10:15:28 -0700

    laurence

--- modules/deleteGame.js
+++ modules/deleteGame.js
@@ -1,4 +1,4 @@
-'use strict' ;
+'use strict';
 
 const { application } = require('express');
 const Game = require('../models/game.js');
@@ -7,7 +7,7 @@ async function deleteGame(request, response, next) {
   try {
     let id = request.params.gameID;
     await Game.findByIdAndDelete(id);
-    
+
     response.status(200).send('Game deleted');
   } catch (error) {
     next(error);
